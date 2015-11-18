d8ecd089a6ea30c3b42427a5e622df3248690870
erick <monge.erick@gmail.com>
2015-11-17 23:50:29 -0600

    test
    test

--- food_alpha_project/src/main/java/com/ulacit/devappweb/service/impl/DishIngredientManagerImpl.java
+++ food_alpha_project/src/main/java/com/ulacit/devappweb/service/impl/DishIngredientManagerImpl.java
@@ -21,7 +21,7 @@ import com.ulacit.devappweb.service.DishIngredientManager;
 import com.ulacit.devappweb.service.IngredientManager;
 
 @Transactional
-@Service("DishIngredientManager")
+@Service("dishIngredientManager")
 @WebService(serviceName = "DishIngredientService", endpointInterface = "com.ulacit.devappweb.DishIngredientManager")
 public class DishIngredientManagerImpl extends GenericManagerImpl<DishIngredient, Long> implements DishIngredientManager {
 	DishIngredientDao dishIngredientDao;
