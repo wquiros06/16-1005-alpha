c9b08b5691bb39ee8e3f8f87b6c57efa501d470b
luigui1692 <LuiGui@LuiGuis-MacBook-Pro.local>
2015-11-17 22:54:11 -0600

    Dish y Review -  Modelo y Dao

---
+++ food_alpha_project/src/main/java/com/ulacit/devappweb/dao/ReviewDao.java
@@ -0,0 +1,13 @@
+package com.ulacit.devappweb.dao;
+ 
+
+
+import com.ulacit.devappweb.model.Review;
+
+import java.util.List;
+
+import org.appfuse.dao.GenericDao;
+
+public interface ReviewDao extends GenericDao<Review, Long> {
+
+}
