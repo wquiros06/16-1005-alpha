32bbb0c4903725b8dc5db1817137f1c3e4630a9c
erick <monge.erick@gmail.com>
2015-11-17 23:11:21 -0600

    push
    puesh

--- food_alpha_project/src/test/java/com/ulacit/devappweb/dao/IngredientDaoTest.java
+++ food_alpha_project/src/test/java/com/ulacit/devappweb/dao/IngredientDaoTest.java
@@ -1,10 +1,11 @@
 package com.ulacit.devappweb.dao;
 
-import org.appfuse.dao.BaseDaoTestCase;
-import org.appfuse.tutorial.model.Person;
+import org.appfuse.dao.BaseDaoTestCase; 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+
+import com.ulacit.devappweb.model.Ingredient;
 
 import java.util.List;
 
@@ -12,46 +13,34 @@ import static org.junit.Assert.*;
 
 public class IngredientDaoTest extends BaseDaoTestCase {
     @Autowired
-    private PersonDao personDao;
+    private IngredientDao ingredientDao;
 
     @Test
-    public void testFindPersonByFirstName() throws Exception {
-        List<Person> people = personDao.findByFirstName("Matt");
-        assertTrue(people.size() > 0);
+    public void testFindIngredientByFirstName() throws Exception {
+        List<Ingredient> ingredients = ingredientDao.findByName("Culantro");
+        assertTrue(ingredients.size() > 0);
     }
     
-    @Test
-    public void testFindPersonByLastName() throws Exception {
-        List<Person> people = personDao.findByLastName("Raible");
-        assertTrue(people.size() > 0);
-    }
-    
-    /*@Test
-    public void testFindPersonByEmail() throws Exception {
-        List<Person> people = personDao.findByEmail("matt@email.com");
-        assertTrue(people.size() > 0);
-    }*/
 
     @Test(expected=DataAccessException.class)
-    public void testAddAndRemovePerson() throws Exception {
-        Person person = new Person();
-        person.setFirstName("Country");
-        person.setLastName("Bry");
+    public void testAddAndRemoveIngredient() throws Exception {
+        Ingredient ingredient = new Ingredient();
+        ingredient.setName("Carne");
 
-        person = personDao.save(person);
+        ingredient = ingredientDao.save(ingredient);
         flush();
 
-        person = personDao.get(person.getPersonId());
+        ingredient = ingredientDao.get(ingredient.getId());
 
-        assertEquals("Country", person.getFirstName());
-        assertNotNull(person.getPersonId());
+        assertEquals("Carne", ingredient.getName());
+        assertNotNull(ingredient.getId());
 
-        log.debug("removing person...");
+        log.debug("removing ingredient...");
 
-        personDao.remove(person.getPersonId());
+        ingredientDao.remove(ingredient.getId());
         flush();
 
         // should throw DataAccessException
-        personDao.get(person.getPersonId());
+        ingredientDao.get(ingredient.getId());
     }
 }
