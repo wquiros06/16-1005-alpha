9468827c8c35ee6d905cd27224a780e96657eb28
erick <monge.erick@gmail.com>
2015-12-13 08:10:13 -0600

    ingredietn manager services

--- food_alpha_project/src/main/java/com/ulacit/devappweb/service/IngredientManager.java
+++ food_alpha_project/src/main/java/com/ulacit/devappweb/service/IngredientManager.java
@@ -10,6 +10,7 @@ import com.ulacit.devappweb.model.Ingredient;
 
 import javax.jws.WebService;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -33,4 +34,20 @@ public interface IngredientManager extends GenericManager<Ingredient, Long> {
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	List<IngredientDTO> getIngredients();
+	
+	@POST
+	@Path("/create/")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	Boolean create(@QueryParam("name") String name);
+	
+	@POST
+	@Path("/update/")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	Boolean create(@QueryParam("id") Long id,@QueryParam("name") String name);
+	
+	@POST
+	@Path("/delete/")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	Boolean create(@QueryParam("id") Long id);
+	
 }
